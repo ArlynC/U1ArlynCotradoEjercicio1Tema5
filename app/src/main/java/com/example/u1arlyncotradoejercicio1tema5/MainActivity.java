d6bc413351bb5ca6d2bce65f61c2c0ca118f2fee
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-01-24 22:39:24 -0500

    muestra mapa

--- app/src/main/java/com/example/u1arlyncotradoejercicio1tema5/MainActivity.java
+++ app/src/main/java/com/example/u1arlyncotradoejercicio1tema5/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.u1arlyncotradoejercicio1tema5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnInfoWindowLongClickListener {
+    GoogleMap mapa;
+    ArrayList<Marker> markers=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+// Obtenemos el mapa de forma asíncrona (notificará cuando esté listo)
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
     }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-18.013766, -70.255331), 14));
+
+    }
+    @Override public void onMapClick(LatLng puntoPulsado) {
+
+    }
+
+
+
+    @Override
+    public void onInfoWindowLongClick(Marker marker) {
+        marker.remove();
+    }
+
 }
+
